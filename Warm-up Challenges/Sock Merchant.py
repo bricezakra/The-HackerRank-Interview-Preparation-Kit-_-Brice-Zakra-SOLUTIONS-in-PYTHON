3fa0f74af86675ba3688c517beae8d3898f608bd
bricezakra <bricezakra@live.com>
2019-10-03 19:04:28 -0500

    Warm-up Challenges folder created, empty 'SOCKET MERCHANT' Problem python file generated

---
+++ Warm-up Challenges/Sock Merchant.py
@@ -0,0 +1,2 @@
+# The-HackerRank-Interview-Preparation-Kit-_-Brice-Zakra-SOLUTIONS-in-PYTHON
+While playing around and testing my CODING SKILLS with PYTHON on HACKERRANK.COM, I will consistently push every single day at least one solution of the INTERVIEW PREPARATION KITCHALLENGE until the last
