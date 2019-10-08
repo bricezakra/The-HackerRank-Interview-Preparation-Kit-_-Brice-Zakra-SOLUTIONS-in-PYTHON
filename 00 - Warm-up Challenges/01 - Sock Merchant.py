13192436f832e18bcc28833df96172637f5cee82
bricezakra <bricezakra@live.com>
2019-10-07 23:32:27 -0500

    SOCK MERCHANT Challenge SOLUTION, ReadMe Updated, ARRAYS folder added

---
+++ 00 - Warm-up Challenges/01 - Sock Merchant.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+
+
+def sockMerchant(n, ar):
+    unique_numbers = list(set(ar))
+    total_pairs = 0
+    for num in unique_numbers:
+        total_pairs += ar.count(num) // 2
+    return total_pairs
+
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     n = int(input())
+
+#     ar = list(map(int, input().rstrip().split()))
+
+#     result = sockMerchant(n, ar)
+
+#     fptr.write(str(result) + '\n')
+
+#     fptr.close()
+
+
+## TEST 00
+n = 9
+ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+print(sockMerchant(n, ar))                                        ### ... output ... 3 (meaning 3 pairs)
+
+                  
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+
 # The-HackerRank-Interview-Preparation-Kit-_-Brice-Zakra-SOLUTIONS-in-PYTHON
-While playing around and testing my CODING SKILLS with PYTHON on HACKERRANK.COM, I will consistently push every single day at least one solution of the INTERVIEW PREPARATION KITCHALLENGE until the last
+
+
+
+
+
+HACKERRANK provided us 69 CODING CHALLNEGES including ARRAYS, DICTIONARIES & HASHMAPS, SORTING, STRING MANIPULATION, GREEDING ALGORITHMS, SEARCH, DYNAMIC PROGRAMMING, STACKS & QUEUES, GRAPHS, TREES, LINKED LISTS and RECURSION & BACKTRACKING, 
+
+
+
+
+
+While playing around and testing my CODING SKILLS in PYTHON on WWW.HACKERRANK.COM, I will consistently push every single day at least one solution of the INTERVIEW-PREPARATION-KIT-CHALLENGE until the last one.
+
