2ee2442e782568827d08bd3dc2d561703f86051d
bricezakra <bricezakra@live.com>
2019-10-16 01:59:05 -0500

    Counting ValleysSolution in Python updated - Big O Notation = O(n)

--- 00 - Warm-up Challenges/02 - Counting Valleys.py
+++ 00 - Warm-up Challenges/02 - Counting Valleys.py
@@ -1,2 +1,47 @@
-# The-HackerRank-Interview-Preparation-Kit-_-Brice-Zakra-SOLUTIONS-in-PYTHON
-While playing around and testing my CODING SKILLS with PYTHON on HACKERRANK.COM, I will consistently push every single day at least one solution of the INTERVIEW PREPARATION KITCHALLENGE until the last
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    level_sea = valley = 0
+    for i in range (n):
+        if (s[i] == "U"):
+            level_sea += 1
+            if (level_sea == 0):
+                valley += 1
+        else:
+            level_sea -= 1
+    return valley
+   
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     n = int(input())
+
+#     s = input()
+
+#     result = countingValleys(n, s)
+
+#     fptr.write(str(result) + '\n')
+
+#     fptr.close()
+
+
+    ## TEST 00
+n = 8
+s = ["U", "D", "D", "D", "U", "D", "U", "U"]
+print(countingValleys(n, s))                                        ### ... output ... 1 (meaning 1 valley)
+
+
+
+
+
+### BIG O NOTATION:
+
+### Time Complexity: O(n);
+### Space Complexity: O(n).
